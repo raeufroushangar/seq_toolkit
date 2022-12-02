50896d80ed1877ee63af3b8b37c7feaebe7c7219
raeufroushangar <roushnroushan19@gmail.com>
2022-12-02 04:39:20 +0000

    Updated code

--- get_sra_data.sh
+++ get_sra_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get SRA ids (first column) from SraRunTable.txt file
-VAR=$(tail -n +2 SraRunTable.txt | cut -d ',' -f 1)
+SRA=$(tail -n +2 SraRunTable.txt | cut -d ',' -f 1)
 
 # This is a loop for downloading the data files
-for i in ${VAR}
+for i in ${SRA}
     do
         echo "(*) Downloading SRA entry: ${i}"
         prefetch ${i}
